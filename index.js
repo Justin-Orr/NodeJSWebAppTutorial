c708b22f9d61ee713cb9333a352d183183da1195
Justin-Orr <jj.oreo98@gmail.com>
2022-01-13 11:40:38 -0600

    Add more npm notes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
  // You will be asked a series of questions about the project. 
  // If you want to quickly generate the file run: npm init --yes
 
+ //---------------------------------------------------------------------------------
+
  /* 1.1 Adding a 3rd party package */
  
  // Go to npmjs.com then find your package then run: npm install package_name
@@ -21,7 +23,48 @@ console.log(result);
 // Mongoose is a package that will allow us to store data in MongoDB so we will install it now to use later.
 // If there are conflicting version dependencies then the package that is dependent on the conflicting version will store itself inside the dependent package.
 
-/* 1.2 Dependency File sizes */
+//---------------------------------------------------------------------------------
+
+/* 1.1.1 Dependency File sizes */
 
 // When uploading your code to git you don't want to include the node_modules folder because it will have hundreds of megabytes worth of code.
 // The package.json in the root folder is there to track the dependencies so they can be downloaded when needed after someone else recieves your code.
+
+//---------------------------------------------------------------------------------
+
+/* 1.1.2 Semantic Versioning */
+
+// When looking at your dependencies you may see a caret symbol. The syntax for version numbers is Major version . Minor version . Patch Version
+// The Minor version is for feature updates. Patch version is for bud fixes. Finally Major versios are for new features that could break the major features of the application.
+
+// The caret character tells npm that you are interested in any version of that dependency as long as the Major version is still that number i.e. the minor and patch versions do not matter.
+
+// If you see a tilde then the Major and Minor versions matter but the patch version do not matter.
+
+//---------------------------------------------------------------------------------
+
+/* 1.1.3 Listing the Installed Packes */
+
+// You can run: 'npm list' which will show all of the dependencies and all of theirs
+// To see the dependencies for just your application run: 'npm list --depth=0'
+
+//---------------------------------------------------------------------------------
+
+/* 1.1.4 Listing meta data for a package */
+
+// You can find all of the meta data about a package by running: npm view module_name
+// This lists the package.json file
+// To see a specific property from this file in a clean way run: npm view module_name property (e.g. npm view mongoose dependencies)
+
+//---------------------------------------------------------------------------------
+
+/* 1.1.4 Installing a specific version of a module */
+
+// Run: npm install module_name@version_number
+
+//---------------------------------------------------------------------------------
+
+/* 1.1.5 Updating a module */
+
+// You can see your outdated packages by running: npm outdated
+// This is determined based on the main registry webpage and finding its current version
